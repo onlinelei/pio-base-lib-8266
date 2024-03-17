8ad72fd89c6700ef056a4807dd322e59662ecdaf
钱磊 <onlinelei@sina.cn>
2024-03-17 23:51:58 +0800

    wif

---
+++ src/main-wifi.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include <InitUtil.h>
+// #include <WifiUtil.h>
+// #include <OTAUtil.h>
+// #include <MyTFTUtils.h>
+
+void setup() {
+    // InitUtil::init();
+    // WifiUtil::wifiInit();
+    // OTAUtil::OTAInitLocalNet();
+    // BlinkUtil::init(testLedArray);
+//    MyTFTUtils::tftInit();
+}
+
+void loop() {
+   Serial.println("something you todo");
+
+    // OTAUtil::OTAHandler();
+    // BlinkUtil::blink(testLedArray);
+//    MyTFTUtils::tftTest();
+//    MyTFTUtils::testFont();
+Serial.println("led blink");
+    delay(1000);
+}
